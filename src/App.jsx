4cb2476a50a42439bf875592b5e1b3af1630618c
Lucas Thépot <lucasthepot13@gmail.com>
2023-04-26 13:56:17 +0100

    trying to add some CSS

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
 
   return (
     <div className="app">
-      <p>{counter}</p>
+      <p className="counterstyle">{counter}</p>
       <Increment counter={counter} setCounter={setCounter} />
       <Decrement counter={counter} setCounter={setCounter} />
-      <Reset counter={counter} setCounter={setCounter} />
+      <Reset className="resetstyle" counter={counter} setCounter={setCounter} />
     </div>
   );
 }
