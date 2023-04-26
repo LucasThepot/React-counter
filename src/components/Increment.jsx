789a5162feba03e7b633377fa9e3b1c21219b54a
Lucas Thépot <lucasthepot13@gmail.com>
2023-04-26 17:26:55 +0100

    still trying to hide buttons + and -

--- src/components/Increment.jsx
+++ src/components/Increment.jsx
@@ -1,15 +1,18 @@
 const Increment = (props) => {
-  return (
-    <button
-      className="decreincrestyle hidebutton"
-      onClick={() => {
-        // Je donne comme nouvelle valeur à counter, se valeur actuelle + 1
-        props.setCounter(props.counter + 1);
-      }}
-    >
-      +
-    </button>
-  );
+  return;
+  {
+    counter < 10 ? (
+      <button
+        className="decreincrestyle"
+        onClick={() => {
+          // Je donne comme nouvelle valeur à counter, se valeur actuelle + 1
+          props.setCounter(props.counter + 1);
+        }}
+      >
+        +
+      </button>
+    ) : null;
+  }
 };
 
 export default Increment;
